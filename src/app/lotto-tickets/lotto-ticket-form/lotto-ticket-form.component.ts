a743dd3d7e68d84adbcf03714d6164deb90a1db1
Nikolas Turunen <nikoltur@gmail.com>
2018-12-12 23:32:28 +0200

    Improved styling of ticket form. Added ticket form numbers to be selectable. Added buy button to be disabled when less than 7 numbers are selected.

--- src/app/lotto-tickets/lotto-ticket-form/lotto-ticket-form.component.ts
+++ src/app/lotto-tickets/lotto-ticket-form/lotto-ticket-form.component.ts
@@ -9,15 +9,40 @@ import { LottoService } from 'src/app/lotto.service';
 })
 export class LottoTicketFormComponent implements OnInit {
 
+    selectedNumbers: Array<Number> = [];
+
     constructor(private lottoService: LottoService) { }
 
     ngOnInit() {
     }
 
+    select(number) {
+        if (this.selectedNumbers.includes(number)) {
+            this.selectedNumbers.splice(this.selectedNumbers.indexOf(number), 1);
+            console.log("Deselected " + number);
+        } else {
+            if (this.selectedNumbers.length < 7) {
+                this.selectedNumbers.push(number);
+
+                console.log("Selected " + number);
+            }
+        }
+    }
+
     purchase() {
-        var ticket = new LottoTicket([1, 2, 3, 4, 5, 6, 7]);
+        if (!this.validate()) {
+            return;
+        }
+
+        var ticket = new LottoTicket(this.selectedNumbers);
 
         this.lottoService.purchaseTicket(ticket);
+
+        this.selectedNumbers = [];
+    }
+
+    validate() {
+        return this.selectedNumbers.length === 7;
     }
 
     array(n: number): any[] {
